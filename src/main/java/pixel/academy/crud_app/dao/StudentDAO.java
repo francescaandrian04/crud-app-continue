5d7e6691f821c3397eda602baeb8ca77247b8649
Francesca Andrian <francescaandrianaaa@gmail.com>
2025-04-24 18:19:01 +0200

    Create a new class in StudentDAOImplementation.

--- src/main/java/pixel/academy/crud_app/dao/StudentDAO.java
+++ src/main/java/pixel/academy/crud_app/dao/StudentDAO.java
@@ -2,11 +2,15 @@ package pixel.academy.crud_app.dao;
 
 import pixel.academy.crud_app.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save (Student theStudent);
 
     Student findbyId(Integer id);
+
+    List<Student> findAll ();
 
 
 
